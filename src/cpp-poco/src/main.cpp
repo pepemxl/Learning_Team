e7ba6d2723ddc8a3f3f5f31959491c334a4f85ae
pepemxl <pepemxl@gmail.com>
2021-08-29 19:12:24 -0500

    Add entrypoint to dockerfile

--- src/cpp-poco/src/main.cpp
+++ src/cpp-poco/src/main.cpp
@@ -8,16 +8,18 @@
  using Poco::Thread;
  using Poco::Stopwatch;
 
- class TimerExample{
- public:
-     TimerExample(){ _sw.start();}
+class TimerExample{
+public:
+    TimerExample(){ 
+        _sw.start();
+    }
 
-     void onTimer(Timer& timer){
-         std::cout << "Callback called after " << _sw.elapsed()/1000 << " milliseconds." << std::endl;
-     }
- private:
-     Stopwatch _sw;
- };
+    void onTimer(Timer& timer){
+        std::cout << "Callback called after " << _sw.elapsed()/1000 << " milliseconds." << std::endl;
+    }
+private:
+    Stopwatch _sw;
+};
 
  int main(int argc, char** argv){
      TimerExample example;
