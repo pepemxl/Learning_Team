3b0b5f2ce1111be88d1efc886313b61bcc729805
Jose Luis Alonzo Velazquez <pepemxl@gmail.com>
2021-12-27 22:56:46 -0600

    docker-compose example

---
+++ src/docker/example_02/docker_build.bat
@@ -0,0 +1,4 @@
+@ECHO OFF
+ECHO "Build Template docker-flask-app-example-02"
+ECHO "docker-compose build"
+docker-compose build
---
+++ src/docker/example_02/docker_stop.bat
@@ -0,0 +1,3 @@
+@ECHO OFF
+ECHO "docker-compose example_02"
+docker-compose stop example_02
